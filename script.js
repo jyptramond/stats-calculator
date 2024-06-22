2ecacda874ca91026a0369c4bd97e6e13a9dfc71
Jonathan Tramond <jonathan.tramond@gmail.com>
2024-06-22 16:59:51 +0200

    amélioration des inputs avec messages d'erreurs

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const button = document.getElementById("generate");
 const result = document.getElementById("resultValue")
+const bonus = document.getElementById("bonus")
+
+
 const dices = document.getElementById("dices")
 const diceType = document.getElementById("diceType");
 const importantDices = document.getElementById("importantDices")
-const bonus = document.getElementById("bonus")
 const times = document.getElementById("times")
+
 launch();
+checkLength(dices,5)
+checkLength(diceType,3)
+checkLength(times,5);
+checkLength(importantDices,5)
 
 
+function checkLength(target,limit) {
+    target.addEventListener('input', function() {
+
+        let value = this.value;
+        console.log(this.value)
+        let isValid = /^\d*$/.test(value);
+        if (!isValid) {
+            this.value = value.replace(/\D/g, '');
+            document.getElementById('error').style.display = 'inline';
+        } else {
+            document.getElementById('error').style.display = 'none';
+        }
+
+
+        if (this.value.length > limit) {
+            this.value = this.value.slice(0, limit);
+        }
+        
+    });
+}
 
 
 function launch() {
@@ -32,17 +59,12 @@ function launch() {
             }
             const total = timesArray.reduce((acc, value) => acc + value, 0);
             const average = total/times.value;
-            const myResult = average.toFixed(3).replace(/\.?0+$/, '');
+            const myResult = average.toFixed(1).replace(/\.?0+$/, '');
             
             result.innerHTML = myResult;
             
         });
 }
-
-
-
-
-
 
 
 
